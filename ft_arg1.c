62b964f1532e29138f462670102847b3b2e96c08
samir-ouaammou <samirouaammou.4.4.2003@gmail.com>
2024-11-05 17:08:43 +0100

    1337 bg

---
+++ ft_arg1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void    ft_print_numbers(int count, ... )
+{
+    va_list list;
+    va_start(list, count);
+
+    while (count > 0)
+    {
+        count--;
+        printf("%d\n", va_arg(list, int));
+    }
+
+    va_end(list);
+}
+
+int main()
+{
+    ft_print_numbers(9,    1, 22, 333, 4444, 55555, 666666, 7777777, 88888888, 999999999);
+    return (0);
+}
