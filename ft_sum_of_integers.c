e88e2c8c2c26a6cc2b425e52fe62bb7d1c474b4b
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2024-11-05 20:56:26 +0100

    ~~~~~

---
+++ ft_sum_of_integers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int	ft_sum_of_integers(int count, ...)
+{
+	va_list		list;
+	int			result;
+
+	va_start(list, count);
+	result = 0;
+	while (count)
+	{
+		 result += va_arg(list, int);
+		count--;
+	}
+	va_end(list);
+	return (result);
+}
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_sum_of_integers(5,	11, 22, 33, 44, 55);
+	printf("%d\n", result);
+
+	return (0);
+}
