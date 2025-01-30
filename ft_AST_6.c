9abedc0d20051dd219d0d3a3cacd1ea03ff8d65b
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-01-30 22:33:22 +0100

    ~~~~~

---
+++ ft_AST_6.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s_ast
+{
+	char			*value;
+	struct s_ast	*left;
+	struct s_ast	*right;
+}					t_ast;
+
+t_ast	*ft_creat_new_node(char *str)
+{
+	t_ast	*new;
+
+	if (!str)
+		return (NULL);
+	new = malloc(sizeof(t_ast));
+	if (!new)
+		return (NULL);
+	new->value = strdup(str);
+	if (!new->value)
+	{
+		free(new);
+		new = NULL;
+		return (NULL);
+	}
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
+
+void	ft_free_ast(t_ast *tree)
+{
+	if (!tree)
+		return ;
+	if (tree->value)
+	{
+		free(tree->value);
+		tree->value = NULL;
+	}
+	ft_free_ast(tree->left);
+	ft_free_ast(tree->right);
+	free(tree);
+	tree = NULL;
+}
+
+t_ast	*ft_build_tree(char **av, int *start, int end)
+{
+	t_ast	*tree;
+
+	if (*start > end)
+		return (NULL);
+	tree = ft_creat_new_node(av[*start]);
+	(*start)++;
+	tree->left = ft_build_tree(av, start, end);
+	(*start)++;
+	tree->right = ft_build_tree(av, start, end);
+	return (tree);
+}
+
+t_ast	*ft_creat_ast_tree(int ac, char **av)
+{
+	int	start;
+
+	start = 1;
+	return (ft_build_tree(av, &start, ac - 1));
+}
+
+void	ft_count_nodes_ast(t_ast *tree, int *len)
+{
+	if (!tree)
+		return ;
+	(*len)++;
+	ft_count_nodes_ast(tree->left, len);
+	ft_count_nodes_ast(tree->right, len);
+}
+
+void	ft_print_count_nodes_ast(t_ast *tree)
+{
+	int	len_ast;
+
+	len_ast = 0;
+	ft_count_nodes_ast(tree, &len_ast);
+	printf("%d\n", len_ast);
+}
+
+int	main(int ac, char **av)
+{
+	t_ast	*tree;
+
+	if (ac < 2)
+		return (-1);
+	tree = ft_creat_ast_tree(ac, av);
+	if (tree)
+	{
+		ft_print_count_nodes_ast(tree);
+		ft_free_ast(tree);
+	}
+	return (0);
+}
